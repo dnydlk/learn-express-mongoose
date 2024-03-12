3d5a142da1a73ff1f3d8c2c572a46a5c55977064
dnydlk <dnydlk97@gmail.com>
2024-03-12 13:23:50 -0400

    Complete quizzes

--- models/author.js
+++ models/author.js
@@ -11,7 +11,9 @@ var AuthorSchema = new Schema(
   }
 );
 
-// Virtual for author's full name
+// Virtual property for author's full name
+//* Not stored in the database, but calculated on the fly
+//* This is an example of OOD of encapsulation. We are encapsulating the logic for computing the full name of the author within the Author model. This is a good practice because it means that if we ever need to change the way we calculate the full name, we only need to change the Author model, and all code that uses the author's full name will automatically use the new method.
 AuthorSchema
 .virtual('name')
 .get(function () {
@@ -27,8 +29,17 @@ AuthorSchema
   return fullname;
 });
 
-// Virtual for author's lifespan
-AuthorSchema.virtual('lifespan').get(function() {});
+// Virtual property for author's lifespan
+AuthorSchema.virtual('lifespan').get(function() {
+  var lifespan_string = '';
+  if (this.date_of_birth && this.date_of_death) {
+    lifespan_string = (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+  }
+  if (!this.date_of_death) {
+    lifespan_string = '';
+  }
+  return lifespan_string;
+});
 
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
